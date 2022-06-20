06808086dd70cf3fbdf25feccf66a2fca6748fc0
danna <dannavila.1809@gmail.com>
2022-06-20 18:39:50 -0500

    animations and webpack configuration

---
+++ index.js
@@ -0,0 +1,18 @@
+// Can also be included with a regular script tag
+import './assets/css/styles.css';
+import AOS from'aos';
+import 'aos/dist/aos.css'; // You can also use <link> for styles
+// ..
+AOS.init({
+    offset: 90,  
+    duration: 1000, 
+     once:true,
+});
+//typing
+import Typed from 'typed.js/src/typed.js';
+var options = {
+    strings: ["hi! i'm", 'danna.'],
+    typeSpeed: 60
+  };
+  
+var typed = new Typed('.element', options);
